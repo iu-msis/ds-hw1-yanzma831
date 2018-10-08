2b53bcba88047e78799cd3969b0337603f8b1acf
Yanzhi Ma <yanzma@iu.edu>
2018-10-08 12:15:15 -0400

    fix error

--- public/api/comment.php
+++ public/api/comment.php
@@ -3,7 +3,7 @@
 require '../../app/common.php';
 
 // 1. Go to the database and get all work associated with the $taskId
-$commentArr = Comment::getAll();
+$commentArr = Comment::fetchAll();
 // 2. Convert to JSON
 $json = json_encode($commentArr, JSON_PRETTY_PRINT);
 // 3. Print
