d29309050acbe58d531d9180d2ad922ac79c3303
Yanzhi Ma <yanzma@iu.edu>
2018-10-08 01:10:33 -0400

    update model file

---
+++ app/models/Comment.php
@@ -0,0 +1,27 @@
+<?php
+class Comment
+{
+  public $id;
+  public $comment;
+  }
+  public static function getCommentByCommentId(int $commentId) {
+    // 1. Connect to the database
+    $db = new PDO(DB_SERVER, DB_USER, DB_PW);
+    // 2. Prepare the query
+    $sql = 'SELECT * FROM Comment WHERE id = ?';
+    $statement = $db->prepare($sql);
+    // 3. Run the query
+    $success = $statement->execute(
+        [$commentId]
+    );
+    // 4. Handle the results
+    $arr = [];
+    while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
+      // 4.a. For each row, make a new work object
+      $commentItem =  new Comment($row);
+      array_push($arr, $commentItem);
+    }
+    return $arr;
+  }
+
+}
