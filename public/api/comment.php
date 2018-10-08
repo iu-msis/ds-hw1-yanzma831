cf65323cb799bd8afb0393087cfc3606750c1de3
Yanzhi Ma <yanzma@iu.edu>
2018-10-08 15:09:46 -0400

    fix error

--- public/api/comment.php
+++ public/api/comment.php
@@ -4,6 +4,7 @@ require '../../app/common.php';
 
 if($_SERVER['REQUEST_METHOD'] == "POST"){
  require 'commentPost.php';
+ exit;
 }
 
 // 1. Go to the database and get all work associated with the $taskId
