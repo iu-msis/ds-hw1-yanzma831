6abad9d8d533e605aa4c27eee7d73ade1eb511a6
Yanzhi Ma <yanzma@iu.edu>
2018-10-15 10:05:59 -0400

    make changes

--- public/js/comments.app.js
+++ public/js/comments.app.js
@@ -14,9 +14,7 @@ var commentsApp = new Vue({
   fetch('api/comment.php')
   .then( response => response.json())
   .then(json =>{
-    commentsApp.comment=json;
-    console.log(json);})
-
+    commentsApp.comment=json;})
   .catch(err =>{
   console.log('Comment Fetch Error:');
   console.log(err);
