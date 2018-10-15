d3ae1dfb32a580a60ced4ac111560c7ff45d1cdd
Yanzhi Ma <yanzma@iu.edu>
2018-10-15 13:39:42 -0400

    post data

--- public/js/comments.app.js
+++ public/js/comments.app.js
@@ -6,9 +6,39 @@ var commentsApp = new Vue({
   comment: ''
 }
 ],
+  commentForm:{}
   },
 
   methods:{
+  handleCommentPost(e){
+  e.preventDefault();
+
+  const s = JSON.stringify(this.commentForm);
+  console.log(s);
+
+  //Post to remote server
+  fetch('api/comment.php',{
+  method: "POST",
+  headers: {
+      "Content-Type": "application/json; charset=utf-8"
+},
+  body:s
+})
+
+  .then( json => {this.comment.push(json)})
+  .catch( err => {
+    console.error('COMMENT POST ERROR:');
+    console.error(err);
+})
+
+  this.commentForm = this.getEmptyCommentForm();
+},
+
+  getEmptyCommentForm(){
+  return{
+    comment:null
+}
+},
 
   fetchComments (){
   fetch('api/comment.php')
